987fd53212d86b1b65792912a5b070b0efdd27f2
Francesco De Bartolomeo <debartolomeo.francesco@gmail.com>
2025-08-30 00:28:13 +0200

    Update main.rs
    chore: add crates to main.rs

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,5 +1,14 @@
 use axum::{routing::get, Router};
+use axum::{
+    extract::{Multipart, Query}, http::{Method, StatusCode}, routing::{get, post}, serve::Listener, Router,
+    response::{Response, IntoResponse},
+};
 
+use std::{
+    env::temp_dir, fs::{File, OpenOptions}, io::{Read, Seek, SeekFrom, Write}, net::TcpListener, string::ParseError
+};
+
+use log::{warn, error, info};
 
 async fn infer() -> &'static str {
     "Hello"
